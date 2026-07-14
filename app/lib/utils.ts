7fc0a84e3daa18c5cb1fa71e49dd71360bdc4894
Artur <arab27696797@gmail.com>
2026-07-14 21:15:27 +0300

    Update utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(value: number, locale: string = 'ru-RU'): string {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: 'RUB',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(value)
+}
+
+export function roundToHundreds(value: number): number {
+  return Math.ceil(value / 100) * 100
+}
